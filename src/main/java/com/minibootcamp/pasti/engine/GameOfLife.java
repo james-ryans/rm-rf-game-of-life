8acdf1489ec75cbf228fe674ca65304591aba5bf
James Ryans <james.ryans2012@gmail.com>
2021-10-02 08:48:41 +0700

    feat: change show method to return actual cells state alive or dead in GameOfLife class

--- src/main/java/com/minibootcamp/pasti/engine/GameOfLife.java
+++ src/main/java/com/minibootcamp/pasti/engine/GameOfLife.java
@@ -1,14 +1,37 @@
 package com.minibootcamp.pasti.engine;
 
+import com.minibootcamp.pasti.grid.Grid;
+
+import java.awt.Point;
+import java.util.List;
+
 public class GameOfLife {
     private int rows, cols;
+    private Grid grid;
 
     public GameOfLife(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
+        this.grid = new Grid(rows, cols);
+    }
+
+    public GameOfLife(int rows, int cols, List<Point> aliveCells) {
+        this(rows, cols);
+
+        for (Point cell : aliveCells) {
+            this.grid.cells[cell.x][cell.y].populate();
+        }
     }
 
     public boolean[][] show() {
-        return new boolean[rows][cols];
+        boolean[][] cells = new boolean[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                cells[i][j] = grid.cells[i][j].isAlive();
+            }
+        }
+
+        return cells;
     }
 }
--- src/test/java/com/minibootcamp/pasti/engine/GameOfLifeTest.java
+++ src/test/java/com/minibootcamp/pasti/engine/GameOfLifeTest.java
@@ -2,6 +2,9 @@ package com.minibootcamp.pasti.engine;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.Point;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class GameOfLifeTest {
@@ -11,6 +14,25 @@ class GameOfLifeTest {
         GameOfLife game = new GameOfLife(3, 3);
 
         boolean[][] expectedCells = new boolean[3][3];
+
+        assertArrayEquals(expectedCells, game.show());
+    }
+
+    @Test
+    void show_Return2DArrayOfTrueBoolean_WhenEveryCellsAreAlive() {
+        List<Point> aliveCells = List.of(
+            new Point(0, 0), new Point(0, 1), new Point(0, 2),
+            new Point(1, 0), new Point(1, 1), new Point(1, 2),
+            new Point(2, 0), new Point(2, 1), new Point(2, 2)
+        );
+        GameOfLife game = new GameOfLife(3, 3, aliveCells);
+
+        boolean[][] expectedCells = new boolean[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                expectedCells[i][j] = true;
+            }
+        }
 
         assertArrayEquals(expectedCells, game.show());
     }
